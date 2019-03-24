8b0d9abe126d6a3f8d84925341f7f77cdc247c9d
himehowareu <himehowareu@gmail.com>
2019-03-23 19:16:48 -0500

    made login and registration pages

--- server.py
+++ server.py
@@ -1,31 +1,45 @@
-from flask import url_for , Flask , request
-from flask import render_template
-import os
+from flask import render_template, Flask, flash
+from flask import redirect, url_for
+from forms import SignupForm, LoginForm
+
 app = Flask(__name__)
 
-domainName="localhost"
-
-def makeLink(address,name):
-    return("<a href='%s'>%s</a>"%(domainName+address,name))
+app.config["SECRET_KEY"] = "shhh_dont_tell_anyone"
 
 @app.route("/")
-def helloW():
-    return "Hello World!<br>to say hi to a person go to %s"%(makeLink(url_for("hello"),"hello"))
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    a=request.args.get('a')
-    return render_template('hello.html', name=name)
-    
-@app.route('/list/')
-def listurl():
-    a=request.args.get('a')
-    return str(os.listdir("."))
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 
-@app.route('/<file>')
-def random(file):
-    return render_template(file)
-    
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/login", methods=["get", "post"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/register", methods=["get", "post"])
+def register():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash(f'account made for {form.name.data}For {form.email.data}', "success")
+        redirect(url_for("index"))
+    return render_template("register.html", title="Register", form=form)
+
 app.run()
