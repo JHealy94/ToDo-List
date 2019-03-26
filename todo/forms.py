f4eef5ad2c8ffe05f4d2927b97e2bf20910fc945
James Healy <47837313+JHealy94@users.noreply.github.com>
2019-03-25 21:24:50 -0500

    split up the server file into smaller files in a package and set up the database model and file

---
+++ todo/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from todo.model import User
+
+
+class SignupForm(FlaskForm):
+    name = StringField('Name',
+                       validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(selfself, email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken, Please Choose a different email.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
---
+++ todo/model.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from todo import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    lists = db.relationship('List', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}', '{self.image_file}"
+
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    items = db.relationship('ListItem', backref='list', lazy=True)
+
+    def __repr__(self):
+        return f"List('{self.title}', {self.date_posted}')"
+
+
+class ListItem(db.Model):
+    list_id = db.Column(db.Integer, db.ForeignKey('list.id'), primary_key=True, nullable=False)
+    content = db.Column(db.String(160), nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Item ('{self.list_id}', '{self.date_added}')"
