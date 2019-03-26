1f18f3bc68ec883e2a28fe98ff85e3190da9d1ca
James Healy <47837313+JHealy94@users.noreply.github.com>
2019-03-25 21:20:34 -0500

    Split up the server file into several smaller python files in a package and set up the database file

--- server.py
+++ server.py
@@ -1,31 +1,4 @@
-from flask import url_for , Flask , request
-from flask import render_template
-import os
-app = Flask(__name__)
-
-domainName="localhost"
-
-def makeLink(address,name):
-    return("<a href='%s'>%s</a>"%(domainName+address,name))
-
-@app.route("/")
-def helloW():
-    return "Hello World!<br>to say hi to a person go to %s"%(makeLink(url_for("hello"),"hello"))
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    a=request.args.get('a')
-    return render_template('hello.html', name=name)
-    
-@app.route('/list/')
-def listurl():
-    a=request.args.get('a')
-    return str(os.listdir("."))
+from todo import app
 
 
-@app.route('/<file>')
-def random(file):
-    return render_template(file)
-    
 app.run()
